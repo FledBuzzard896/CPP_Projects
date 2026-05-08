6523f33e9fa1a2cd43230deb819159ac815be4b4
Anton Solodov <sobaken334@gmail.com>
2026-05-08 21:17:28 +0300

    Продумать алгоритм генерации лабиринта

---
+++ Project Labyrinth/Project Labyrinth.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+class Labyrynth 
+{
+public:
+    void GenerateLabyrynth(int inputLabSize)
+    {
+        // Задаём размер из возможного диапазона
+        int SIZE = clamp(inputLabSize, 10, 100);
+        vector<vector<int>> LABYRYNTH(SIZE, vector<int>(SIZE));
+
+        // ВХОД - ВЫХОД
+        LABYRYNTH[0][0] = 2;
+        LABYRYNTH[SIZE][SIZE] = 2;
+
+        //Генерация путей и стенок
+    }
+};
+
+int main()
+{
+    cout << "Hello World!\n";
+}
